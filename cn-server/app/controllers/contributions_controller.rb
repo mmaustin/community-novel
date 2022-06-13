6bc57125d11fd96ad2141099d1ea13cc599b3a60
McCray Austin <47298611+mmaustin@users.noreply.github.com>
2022-06-13 08:06:28 -0400

    created dry restful routes in the contributions controller

--- cn-server/app/controllers/contributions_controller.rb
+++ cn-server/app/controllers/contributions_controller.rb
@@ -1,2 +1,18 @@
 class ContributionsController < ApplicationController
+
+    def index
+    end
+
+    def show
+    end
+
+    def create
+    end
+
+    def update
+    end
+
+    def destroy
+    end
+    
 end
