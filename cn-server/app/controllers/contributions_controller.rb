840aefaa5d4e972dfdc41d2cc17ca7534d4c86d2
McCray Austin <47298611+mmaustin@users.noreply.github.com>
2022-06-13 16:19:12 -0400

    copied works controller code to contributions controller

--- cn-server/app/controllers/contributions_controller.rb
+++ cn-server/app/controllers/contributions_controller.rb
@@ -1,18 +1,43 @@
 class ContributionsController < ApplicationController
 
+    before_action :set_work, only: [:show, :update, :destroy]    
+
     def index
+        works = Work.all
+        render json: works.to_json(only: [:id, :title, :genre, :contributions], :include => {:contributions => {only: [:id, :author_id]}})
     end
 
     def show
+        render json: @work.to_json(only: [:id, :title, :genre, :contributions], :include => {:contributions => {only: [:id, :author_id]}})
     end
 
     def create
+        @work = Work.new(work_params)
+    
+        if @work.save
+          render json: @work, status: :created, location: @worker
+        else
+          render json: @work.errors, status: :unprocessable_entity
+        end
     end
 
     def update
+        @work.update(work_params)
+        render json: @work       
     end
 
     def destroy
+        @work.destroy
     end
+
+    private
+
+    def set_work
+        @work = Work.find(params[:id])
+    end
+
+    def work_params
+        params.require(:work).permit(:title, :genre, :contributions, :author_id)
+    end 
     
 end
