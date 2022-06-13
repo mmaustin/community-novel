f74f50c35c1cce21027ade31dc5d8dc46b62d0df
McCray Austin <47298611+mmaustin@users.noreply.github.com>
2022-06-13 09:01:19 -0400

    added render json information in works index route

--- cn-server/app/controllers/works_controller.rb
+++ cn-server/app/controllers/works_controller.rb
@@ -1,6 +1,8 @@
 class WorksController < ApplicationController
 
     def index
+        works = Work.all
+        render json: works.to_json(only: [:id, :title, :genre, :contributions], :include => {:contributions => {only: [:id, :author_id]}})
     end
 
     def show
