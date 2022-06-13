72742e38b4e8b31d7844db69df6e142d3b89f6cf
McCray Austin <47298611+mmaustin@users.noreply.github.com>
2022-06-13 09:42:55 -0400

    built out the create update and destroy routes in the works controller

--- cn-server/app/controllers/works_controller.rb
+++ cn-server/app/controllers/works_controller.rb
@@ -8,15 +8,26 @@ class WorksController < ApplicationController
     end
 
     def show
+        render json: @work.to_json(only: [:id, :title, :genre, :contributions], :include => {:contributions => {only: [:id, :author_id]}})
     end
 
     def create
+        @work = Work.new(work_params)
+    
+        if @work.save
+          render json: @work, status: :created, location: @worker
+        else
+          render json: @work.errors, status: :unprocessable_entity
+        end
     end
 
     def update
+        @work.update(work_params)
+        render json: @work       
     end
 
     def destroy
+        @work.destroy
     end
 
     private
