bb1bca480c64ff2faceaa2d5d22d21ef3ea505c7
McCray Austin <47298611+mmaustin@users.noreply.github.com>
2022-06-14 12:08:49 -0400

    created the data service class for the contributions server routes

---
+++ cn-client/src/services/contributionService.js
@@ -0,0 +1,20 @@
+import http from "../http-common";
+
+class DataServiceC {
+    getAll() {
+        return http.get("/contributions");
+    }
+    create(band) {
+        return http.post("/contributions", band);
+    }
+    get(id) {
+        return http.get(`/contributions/${id}`);
+    }
+    delete(id) {
+        return http.delete(`/contributions/${id}`);
+    }
+    update(id, data) {
+        return http.put(`/contributions/${id}`, data);
+    }
+}
+export default new DataServiceC();
