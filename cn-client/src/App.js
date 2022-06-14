352bc0c8797f676cb8c7d29a36a5038d91dec149
McCray Austin <47298611+mmaustin@users.noreply.github.com>
2022-06-14 13:54:43 -0400

    created a fetch request in app component to test connection with the server

--- cn-client/src/App.js
+++ cn-client/src/App.js
@@ -1,7 +1,16 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 
 
 const App = () => {
+
+  useEffect(() => {
+    fetch("http://localhost:3000/authors").then((res) => {
+        res.json().then((data) => {
+          console.log(data)
+        });
+    });
+  }, []);
+
   return (
     <>
       <p>What's up, Doc!</p>
