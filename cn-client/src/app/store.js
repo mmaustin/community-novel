05ecb4c6322a93d9b4d13c70680a8b7fcc9682d0
McCray Austin <47298611+mmaustin@users.noreply.github.com>
2022-06-15 12:00:12 -0400

    added work slice to the store and set up fetch of works in index file

--- cn-client/src/app/store.js
+++ cn-client/src/app/store.js
@@ -1,8 +1,10 @@
 import { configureStore } from '@reduxjs/toolkit';
 import authorSlice from '../features/authors/authorSlice';
+import workSlice from '../features/works/workSlice';
 
 export const store = configureStore({
   reducer: {
     authors: authorSlice,
+    works: workSlice,
   },
 });
--- cn-client/src/index.js
+++ cn-client/src/index.js
@@ -4,11 +4,13 @@ import { Provider } from 'react-redux';
 import { store } from './app/store';
 import App from './App';
 import { fetchAuthors } from './features/authors/authorSlice';
+import { fetchWorks } from './features/works/workSlice';
 
 const container = document.getElementById('root');
 const root = createRoot(container);
 
 store.dispatch(fetchAuthors());
+store.dispatch(fetchWorks());
 
 root.render(
   //<React.StrictMode>
