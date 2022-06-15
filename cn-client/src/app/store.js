25cf804e292730c46df42f8082552a16c57bf41c
McCray Austin <47298611+mmaustin@users.noreply.github.com>
2022-06-15 10:16:05 -0400

    configured store to accept authors slice

--- cn-client/src/app/store.js
+++ cn-client/src/app/store.js
@@ -1,8 +1,8 @@
 import { configureStore } from '@reduxjs/toolkit';
-import counterReducer from '../features/counter/counterSlice';
+import authorSlice from '../features/authors/authorSlice';
 
 export const store = configureStore({
   reducer: {
-    counter: counterReducer,
+    authors: authorSlice,
   },
 });
