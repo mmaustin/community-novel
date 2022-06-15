ea0bc20c9974452418346390887b1c1af25b5f39
McCray Austin <47298611+mmaustin@users.noreply.github.com>
2022-06-15 15:41:19 -0400

    imported contribution slice to the store

--- cn-client/src/app/store.js
+++ cn-client/src/app/store.js
@@ -1,10 +1,12 @@
 import { configureStore } from '@reduxjs/toolkit';
 import authorSlice from '../features/authors/authorSlice';
 import workSlice from '../features/works/workSlice';
+import contributionSlice from '../features/contributions/contributionSlice';
 
 export const store = configureStore({
   reducer: {
     authors: authorSlice,
     works: workSlice,
+    contributions: contributionSlice
   },
 });
