b8a0a6cca4602c676371944600408756ce7e3b3e
McCray Austin <47298611+mmaustin@users.noreply.github.com>
2022-06-16 11:29:49 -0400

    corrected a render json error in authors controller

--- cn-server/app/controllers/authors_controller.rb
+++ cn-server/app/controllers/authors_controller.rb
@@ -4,11 +4,11 @@ class AuthorsController < ApplicationController
 
     def index
         authors = Author.all
-        render json: authors.to_json(only: [:id, :name, :statement], :include => {:works => {only: [:id, :title, :genre, :contributions]}})
+        render json: authors.to_json(only: [:id, :name, :statement], :include => {:works => {only: [:id, :title, :genre, :contribution_number]}})
     end
 
     def show
-        render json: @author.to_json(only: [:id, :name, :statement], :include => {:works => {only: [:id, :title, :genre, :contributions]}})
+        render json: @author.to_json(only: [:id, :name, :statement], :include => {:works => {only: [:id, :title, :genre, :contribution_number]}})
     end
 
     def create
