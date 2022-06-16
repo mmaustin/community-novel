b5a5d3195e81f260b088f97a09a7dd8ef2e17d78
McCray Austin <47298611+mmaustin@users.noreply.github.com>
2022-06-16 11:20:40 -0400

    added author works component to author list rendering

--- cn-client/src/features/authors/AuthorsList.js
+++ cn-client/src/features/authors/AuthorsList.js
@@ -2,13 +2,14 @@ import React, {useEffect} from 'react';
 import {useDispatch, useSelector} from 'react-redux';
 import { allAuthors} from './authorSlice';
 import { fetchAuthors } from './authorSlice';
+import { AuthorWorks } from './AuthorWorks';
 
 export const AuthorsList = () => {
 
     const dispatch = useDispatch();
     const authors = useSelector(allAuthors);
     const authorsStatus = useSelector(state => state.authors.status);
-    
+
     useEffect(() => {
         if (authorsStatus === 'idle'){
             dispatch(fetchAuthors());
@@ -19,7 +20,10 @@ export const AuthorsList = () => {
 
     if (authorsStatus === 'succeeded'){
         content = authors.map((author, i) => {
-            return <div key={i}><p>{author.name}</p></div>
+            return <div key={i}>
+                <p>{author.name}</p>
+                <AuthorWorks works={author.works}/>
+            </div>
         })
     }
 
