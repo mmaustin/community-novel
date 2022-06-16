ce3275fc56301f84f067318e6f99b93de43de8ac
McCray Austin <47298611+mmaustin@users.noreply.github.com>
2022-06-16 10:30:14 -0400

    imported authors list component to app and gave it a route

--- cn-client/src/App.js
+++ cn-client/src/App.js
@@ -1,12 +1,14 @@
 import React from 'react';
+import {BrowserRouter as Router, Routes, Route} from 'react-router-dom'
+import { AuthorsList } from './features/authors/AuthorsList';
 
 const App = () => {
-
-
   return (
-    <>
-      <p>What's up, Doc!</p>
-    </>
+    <Router>
+      <Routes>
+      <Route exact path='/' element={< AuthorsList />} />
+      </Routes>
+    </Router>
   );
 }
 
