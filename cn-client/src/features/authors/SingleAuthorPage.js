0ecfb146c87c601f12a1f4019eaa5d52565fcf00
McCray Austin <47298611+mmaustin@users.noreply.github.com>
2022-06-18 13:36:19 -0400

    coded the non-return portion of single author page

---
+++ cn-client/src/features/authors/SingleAuthorPage.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import { useSelector} from 'react-redux'
+import { useParams} from 'react-router-dom';
+import { getAuthorById } from './authorSlice';
+
+export const SingleAuthorPage = () => {
+
+    const params = useParams();
+    const authorId = parseInt(params.authorId);
+
+    const author = useSelector(state => getAuthorById(state, authorId));
+
+    return(
+        <></>
+    )
+}
