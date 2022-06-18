d475992689afcf1c3bfe98ec4c250ed29d4f6c9c
McCray Austin <47298611+mmaustin@users.noreply.github.com>
2022-06-18 11:42:47 -0400

    gave add author form component a route in app component

--- cn-client/src/App.js
+++ cn-client/src/App.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import {BrowserRouter as Router, Routes, Route} from 'react-router-dom'
 import { AuthorsList } from './features/authors/AuthorsList';
+import { AddAuthorForm } from './features/authors/AddAuthorForm';
 
 const App = () => {
   return (
     <Router>
       <Routes>
       <Route exact path='/' element={< AuthorsList />} />
+      <Route exact path='/add-author' element={< AddAuthorForm />} />
       </Routes>
     </Router>
   );
