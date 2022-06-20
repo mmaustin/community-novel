98473b5f3a50c4bfbabef787f5554067f692b002
McCray Austin <47298611+mmaustin@users.noreply.github.com>
2022-06-20 13:24:08 -0400

    imported dispatch, navigate, and delete author action to single author page component

--- cn-client/src/features/authors/SingleAuthorPage.js
+++ cn-client/src/features/authors/SingleAuthorPage.js
@@ -1,14 +1,23 @@
 import React from 'react'
-import { useSelector} from 'react-redux'
-import { useParams, Link} from 'react-router-dom';
+import { useSelector, useDispatch} from 'react-redux'
+import { useParams, Link, useNavigate} from 'react-router-dom';
 import { getAuthorById } from './authorSlice';
+import { deleteAuthor } from './authorSlice';
 
 export const SingleAuthorPage = () => {
+
+    const dispatch = useDispatch();
+    const navigate = useNavigate();
 
     const params = useParams();
     const authorId = parseInt(params.authorId);
 
     const author = useSelector(state => getAuthorById(state, authorId));
+
+    const deleteThisAuthor = () => {
+        dispatch(deleteAuthor({id: author.id}));
+        navigate('/');
+    }
 
     return(
         <>
