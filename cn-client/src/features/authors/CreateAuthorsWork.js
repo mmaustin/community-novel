401262f1ba682450fde427aa85c1dda64844585f
McCray Austin <47298611+mmaustin@users.noreply.github.com>
2022-06-21 12:32:20 -0400

    added the non-form code in create author works component

--- cn-client/src/features/authors/CreateAuthorsWork.js
+++ cn-client/src/features/authors/CreateAuthorsWork.js
@@ -1,5 +1,6 @@
 import React, {useState} from 'react';
 import { useDispatch } from 'react-redux';
+import { createWork } from '../works/workSlice';
 
 export const CreateAuthorsWork = ({author}) => {
 
@@ -7,6 +8,30 @@ export const CreateAuthorsWork = ({author}) => {
     const [title, setTitle] = useState('');
     const [genre, setGenre] = useState('');
     const [contributions, setContributions] = useState(0);
+    const [addRequestStatus, setAddRequestStatus] = useState('idle')    
+
+    const onChangeTitle = (e) => setTitle(e.target.value);
+    const onChangeGenre = (e) => setGenre(e.target.value);
+    const onChangeContributions = (e) => setContributions(e.target.value);
+    
+    const canSave =
+    [title, genre, contributions].every(Boolean) && addRequestStatus === 'idle'
+    
+    const onSaveWorkClicked = async () => {
+        if (canSave) {
+          try {
+            setAddRequestStatus('pending')
+            await dispatch(createWork({work: { title, genre, contribution_number: contributions, author_id: author.id }})).unwrap()
+            setTitle('');
+            setGenre('');
+            setContributions('');
+          } catch (err) {
+            console.error('Failed to save the post: ', err)
+          } finally {
+            setAddRequestStatus('idle')
+          }
+        }
+      }    
 
     return(
         <>
