8fc2bd90d266c1d69d8787e16cdf95478097dba5
McCray Austin <47298611+mmaustin@users.noreply.github.com>
2022-06-21 12:04:51 -0400

    imported use state and dispatch into create author works component

--- cn-client/src/features/authors/CreateAuthorsWork.js
+++ cn-client/src/features/authors/CreateAuthorsWork.js
@@ -1,7 +1,12 @@
-import React from 'react';
+import React, {useState} from 'react';
+import { useDispatch } from 'react-redux';
 
 export const CreateAuthorsWork = ({author}) => {
 
+    const dispatch = useDispatch();
+    const [title, setTitle] = useState('');
+    const [genre, setGenre] = useState('');
+    const [contributions, setContributions] = useState(0);
 
     return(
         <>
