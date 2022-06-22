83441ec8eb7f9d8e0399e13358310f67dae2f417
McCray Austin <47298611+mmaustin@users.noreply.github.com>
2022-06-22 17:02:31 -0400

    added a route to works list in the app component

--- cn-client/src/App.js
+++ cn-client/src/App.js
@@ -4,12 +4,14 @@ import { AuthorsList } from './features/authors/AuthorsList';
 import { AddAuthorForm } from './features/authors/AddAuthorForm';
 import { SingleAuthorPage } from './features/authors/SingleAuthorPage';
 import { UpdateAuthor } from './features/authors/UpdateAuthor';
+import { WorksList } from './features/works/WorksList';
 
 const App = () => {
   return (
     <Router>
       <Routes>
       <Route exact path='/' element={< AuthorsList />} />
+      <Route exact path='/all-works' element={< WorksList />} />
       <Route exact path='/add-author' element={< AddAuthorForm />} />
       <Route exact path='/get-author/:authorId' element={<SingleAuthorPage />} />
       <Route exact path='/update-author/:authorId' element={<UpdateAuthor />} />      
