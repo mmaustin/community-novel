ff80b11e9e5e62a427ee746ac02ab9ce3fdbbeee
McCray Austin <47298611+mmaustin@users.noreply.github.com>
2022-06-22 17:00:24 -0400

    built out works list component

--- cn-client/src/features/works/WorksList.js
+++ cn-client/src/features/works/WorksList.js
@@ -1,11 +1,20 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
+//import { Link } from 'react-router-dom';
+import { allWorks } from './workSlice';
 
 export const WorksList = () => {
 
-
+    const worksFromStore = useSelector(allWorks);
+    const theWorks = worksFromStore.map((work, i) => {
+        return <div key={i}>
+                <p>{work.title}</p>
+        </div>
+    })
 
     return(
         <>
+            {theWorks}
         </>
     )
 }
