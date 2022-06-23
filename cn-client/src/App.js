fe8d490ba54d53bef4811fc80103bc943ac1632b
McCray Austin <47298611+mmaustin@users.noreply.github.com>
2022-06-23 13:10:55 -0400

    supplied single work page component with a route in app component

--- cn-client/src/App.js
+++ cn-client/src/App.js
@@ -5,6 +5,7 @@ import { AddAuthorForm } from './features/authors/AddAuthorForm';
 import { SingleAuthorPage } from './features/authors/SingleAuthorPage';
 import { UpdateAuthor } from './features/authors/UpdateAuthor';
 import { WorksList } from './features/works/WorksList';
+import { SingleWorkPage } from './features/works/SingleWorkPage';
 
 const App = () => {
   return (
@@ -14,7 +15,8 @@ const App = () => {
       <Route exact path='/all-works' element={< WorksList />} />
       <Route exact path='/add-author' element={< AddAuthorForm />} />
       <Route exact path='/get-author/:authorId' element={<SingleAuthorPage />} />
-      <Route exact path='/update-author/:authorId' element={<UpdateAuthor />} />      
+      <Route exact path='/update-author/:authorId' element={<UpdateAuthor />} />
+      <Route exact path='/get-work/:workId' element={<SingleWorkPage />} />           
       </Routes>
     </Router>
   );
