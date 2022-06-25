df5d5506185d69da5f1707af51fe014bd8eeaa8c
McCray Austin <47298611+mmaustin@users.noreply.github.com>
2022-06-25 12:15:45 -0400

    created shell of add contribution component

---
+++ cn-client/src/features/contributions/AddContribution.js
@@ -0,0 +1,13 @@
+import React, {useState} from 'react';
+import { useDispatch } from 'react-redux';
+import { createContribution} from './contributionSlice';
+
+export const AddContribution = () => {
+
+
+
+    return(
+        <>
+        </>
+    )
+}
