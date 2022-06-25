1c7bfe6f70bb410375c12b9b6503118d0e340139
McCray Austin <47298611+mmaustin@users.noreply.github.com>
2022-06-25 13:01:35 -0400

    built out the non-return portion of add contribution component

--- cn-client/src/features/contributions/AddContribution.js
+++ cn-client/src/features/contributions/AddContribution.js
@@ -4,11 +4,33 @@ import { createContribution} from './contributionSlice';
 
 export const AddContribution = ({work}) => {
 
+    const [text, setText] = useState('');
+    const [addRequestStatus, setAddRequestStatus] = useState('idle')
+    
+    const dispatch = useDispatch();
+    
+    const onTextChanged = (e) => setText(e.target.value);
 
+    const canSave =
+    [text].every(Boolean) && addRequestStatus === 'idle'
+
+    const saveContribution = async () => {
+        if (canSave) {
+          try {
+            setAddRequestStatus('pending')
+            await dispatch(createContribution({contribution: { text, author_id: work.author_id, work_id: work.id }})).unwrap()
+            setText('');
+          } catch (err) {
+            console.error('Failed to save the contribution: ', err)
+          } finally {
+            setAddRequestStatus('idle')
+          }
+        }
+      }
 
     return(
         <>
-            <p>{work.id}</p>
+            <p>{work.author_id} and {work.id}</p>
         </>
     )
 }
