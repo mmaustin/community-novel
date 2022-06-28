8e1124424a9659436cd67c6d1d2dfffef1b5d2c1
McCray Austin <47298611+mmaustin@users.noreply.github.com>
2022-06-28 11:32:03 -0400

    added a route for the homepage component in app

--- cn-client/src/App.js
+++ cn-client/src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import {BrowserRouter as Router, Routes, Route} from 'react-router-dom'
+import { Homepage } from './features/Homepage';
 import { AuthorsList } from './features/authors/AuthorsList';
 import { AddAuthorForm } from './features/authors/AddAuthorForm';
 import { SingleAuthorPage } from './features/authors/SingleAuthorPage';
@@ -11,12 +12,13 @@ const App = () => {
   return (
     <Router>
       <Routes>
-      <Route exact path='/' element={< AuthorsList />} />
+      <Route exact path='/' element={< Homepage />} />      
+      <Route exact path='/all-authors' element={< AuthorsList />} />
       <Route exact path='/all-works' element={< WorksList />} />
       <Route exact path='/add-author' element={< AddAuthorForm />} />
       <Route exact path='/get-author/:authorId' element={<SingleAuthorPage />} />
       <Route exact path='/update-author/:authorId' element={<UpdateAuthor />} />
-      <Route exact path='/get-work/:workId' element={<SingleWorkPage />} />           
+      <Route exact path='/get-work/:workId' element={<SingleWorkPage />} />
       </Routes>
     </Router>
   );
