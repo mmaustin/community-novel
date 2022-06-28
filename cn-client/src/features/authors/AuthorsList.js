9591503cbd96846c218ce68b4ac88c171104e38a
McCray Austin <47298611+mmaustin@users.noreply.github.com>
2022-06-28 15:33:47 -0400

    removed all dispatching and fetching capabilities from the authors list component

--- cn-client/src/features/authors/AuthorsList.js
+++ cn-client/src/features/authors/AuthorsList.js
@@ -1,33 +1,17 @@
-import React, {useEffect} from 'react';
-import {useDispatch, useSelector} from 'react-redux';
+import React from 'react';
+import {useSelector} from 'react-redux';
 import { allAuthors} from './authorSlice';
-import { fetchAuthors } from './authorSlice';
-import { fetchWorks } from '../works/workSlice';
 import { AuthorWorks } from './AuthorWorks';
 //import { AuthorContributions } from './AuthorContributions';
-import { fetchContributions } from '../contributions/contributionSlice';
+
 import { Link } from 'react-router-dom';
 
 export const AuthorsList = () => {
 
-    const dispatch = useDispatch();
     const authors = useSelector(allAuthors);
-    const authorsStatus = useSelector(state => state.authors.status);
-    const worksStatus = useSelector(state => state.works.status);
-    const contributionsStatus = useSelector(state => state.contributions.status);
 
-    useEffect(() => {
-        if (authorsStatus === 'idle' && worksStatus === 'idle' && contributionsStatus === 'idle'){
-            dispatch(fetchAuthors());
-            dispatch(fetchWorks());
-            dispatch(fetchContributions());
-        }
-    }, [authorsStatus, worksStatus, contributionsStatus, dispatch])
 
-    let content;
-
-    if (authorsStatus === 'succeeded'){
-        content = authors.map((author, i) => {
+        const content = authors.map((author, i) => {
             return <div key={i}>
                 <p>{author.name}</p>
                 <AuthorWorks works={author.works}/>
@@ -35,7 +19,7 @@ export const AuthorsList = () => {
                 <Link to={`/get-author/${author.id}`} className="author-link">Single Author</Link>
             </div>
         })
-    }
+
 
     return(
         <>
