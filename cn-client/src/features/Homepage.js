78e0e1a99d5722ce51ca5cd3176b3b08ddd5c72a
McCray Austin <47298611+mmaustin@users.noreply.github.com>
2022-06-28 11:29:26 -0400

    imported code to fetch resources upon the homepage's loading

--- cn-client/src/features/Homepage.js
+++ cn-client/src/features/Homepage.js
@@ -1,8 +1,23 @@
-import React from 'react';
+import React, {useEffect} from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { fetchAuthors } from './authorSlice';
+import { fetchWorks } from '../works/workSlice';
+import { fetchContributions } from '../contributions/contributionSlice';
 
 export const Homepage = () => {
 
+    const dispatch = useDispatch();
+    const authorsStatus = useSelector(state => state.authors.status);
+    const worksStatus = useSelector(state => state.works.status);
+    const contributionsStatus = useSelector(state => state.contributions.status);
 
+    useEffect(() => {
+        if (authorsStatus === 'idle' && worksStatus === 'idle' && contributionsStatus === 'idle'){
+            dispatch(fetchAuthors());
+            dispatch(fetchWorks());
+            dispatch(fetchContributions());
+        }
+    }, [authorsStatus, worksStatus, contributionsStatus, dispatch])
     
     return(
         <>
