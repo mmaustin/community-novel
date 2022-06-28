245f0594663ecfba1077347ad72853cafde7c59a
McCray Austin <47298611+mmaustin@users.noreply.github.com>
2022-06-28 15:45:09 -0400

    imported navbar and added it to router in app component

--- cn-client/src/App.js
+++ cn-client/src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import {BrowserRouter as Router, Routes, Route} from 'react-router-dom'
+import { Navbar } from './app/Navbar';
 import { Homepage } from './features/Homepage';
 import { AuthorsList } from './features/authors/AuthorsList';
 import { AddAuthorForm } from './features/authors/AddAuthorForm';
@@ -11,15 +12,16 @@ import { SingleWorkPage } from './features/works/SingleWorkPage';
 const App = () => {
   return (
     <Router>
-      <Routes>
-      <Route exact path='/' element={< Homepage />} />      
-      <Route exact path='/all-authors' element={< AuthorsList />} />
-      <Route exact path='/all-works' element={< WorksList />} />
-      <Route exact path='/add-author' element={< AddAuthorForm />} />
-      <Route exact path='/get-author/:authorId' element={<SingleAuthorPage />} />
-      <Route exact path='/update-author/:authorId' element={<UpdateAuthor />} />
-      <Route exact path='/get-work/:workId' element={<SingleWorkPage />} />
-      </Routes>
+      <Navbar />
+        <Routes>
+          <Route exact path='/' element={< Homepage />} />      
+          <Route exact path='/all-authors' element={< AuthorsList />} />
+          <Route exact path='/all-works' element={< WorksList />} />
+          <Route exact path='/add-author' element={< AddAuthorForm />} />
+          <Route exact path='/get-author/:authorId' element={<SingleAuthorPage />} />
+          <Route exact path='/update-author/:authorId' element={<UpdateAuthor />} />
+          <Route exact path='/get-work/:workId' element={<SingleWorkPage />} />
+        </Routes>
     </Router>
   );
 }
