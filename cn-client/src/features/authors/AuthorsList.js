371715c6fccdacf64997eb31fd2a0b9b9f15c441
McCray Austin <47298611+mmaustin@users.noreply.github.com>
2022-06-29 13:29:49 -0400

    added dispatch of change color action to authors list component

--- cn-client/src/features/authors/AuthorsList.js
+++ cn-client/src/features/authors/AuthorsList.js
@@ -1,21 +1,26 @@
 import React from 'react';
-import {useSelector} from 'react-redux';
+import {useSelector, useDispatch} from 'react-redux';
 import { allAuthors} from './authorSlice';
 import { AuthorWorks } from './AuthorWorks';
+import { changeColor } from './authorSlice';
 //import { AuthorContributions } from './AuthorContributions';
 
 import { Link } from 'react-router-dom';
 
 export const AuthorsList = () => {
 
+    const dispatch = useDispatch();
+
     const authors = useSelector(allAuthors);
     const lowercaseAuthors = authors.slice().sort((a,b) => b.name.localeCompare(a.name))
 
-
+    const changeTextColor = (e) => {
+        dispatch(changeColor(e))
+    }
 
     const content = lowercaseAuthors.map((author, i) => {
         return <div key={i}>
-            <p>{author.name}</p>
+            <p onClick={e=>changeTextColor(e)}>{author.name}</p>
             <AuthorWorks works={author.works}/>
             {/*<AuthorContributions contributions={author.contributions}/>*/}
             <Link to={`/get-author/${author.id}`} className="author-link">Single Author</Link>
