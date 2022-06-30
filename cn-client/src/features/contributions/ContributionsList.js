2b02e3cf687f335a910da3f9a8afdd3489e21453
McCray Austin <47298611+mmaustin@users.noreply.github.com>
2022-06-30 12:38:11 -0400

    created shell component of contributions list

---
+++ cn-client/src/features/contributions/ContributionsList.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { useSelector } from 'react-redux';
+import { allContributions } from './contributionSlice';
+
+export const ContributionsList = () => {
+
+
+
+    return(
+        <>
+        </>
+    )
+}
