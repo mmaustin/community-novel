01b54a41a91baf47b6e197a4b401ea5bea2d92dd
McCray Austin <47298611+mmaustin@users.noreply.github.com>
2022-06-30 12:52:32 -0400

    displayed contribution text in contribution list component

--- cn-client/src/features/contributions/ContributionsList.js
+++ cn-client/src/features/contributions/ContributionsList.js
@@ -4,10 +4,15 @@ import { allContributions } from './contributionSlice';
 
 export const ContributionsList = () => {
 
+    const contributions = useSelector(allContributions);
 
+    const contributionsList = contributions.map((cont, i) => {
+        return <p key={i}>{cont.text}</p>
+    })
 
     return(
-        <>
-        </>
+        <div>
+            {contributionsList}
+        </div>
     )
 }
