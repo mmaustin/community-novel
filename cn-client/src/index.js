c22c98edc771327304c50c31da68e070d785fc84
McCray Austin <47298611+mmaustin@users.noreply.github.com>
2022-07-06 16:16:49 -0400

    added index.css file

--- cn-client/src/index.js
+++ cn-client/src/index.js
@@ -3,6 +3,7 @@ import { createRoot } from 'react-dom/client';
 import { Provider } from 'react-redux';
 import { store } from './app/store';
 import App from './App';
+
 //import { fetchAuthors } from './features/authors/authorSlice';
 //import { fetchWorks } from './features/works/workSlice';
 
