f51dab29dd1520a31b1d62f75641ebc2c1c99578
McCray Austin <47298611+mmaustin@users.noreply.github.com>
2022-07-07 16:49:18 -0400

    added a button and new classnames to navbar and its tags

--- cn-client/src/app/Navbar.js
+++ cn-client/src/app/Navbar.js
@@ -6,11 +6,14 @@ export const Navbar = () =>{
     return(
         <nav id='navbar'>
             <h4 id='nav-header'>Links to . . .</h4>
-            <div id='nav-link-container'>
-                <Link className='component-link' to='/'>Home Page</Link>
-                <Link className='component-link' to='/all-authors'>The Authors</Link>
-                <Link className='component-link' to='/all-works'>The Works</Link>
-                <Link className='component-link' to='/all-contributions'>The Contributions</Link>                
+            <div className='dropdown'>
+                <button className='dropbtn'>Menu</button>
+                <div className='dropdown-content'>
+                    <Link className='component-link' to='/'>Home Page</Link>
+                    <Link className='component-link' to='/all-authors'>The Authors</Link>
+                    <Link className='component-link' to='/all-works'>The Works</Link>
+                    <Link className='component-link' to='/all-contributions'>The Contributions</Link>                
+                </div>
             </div>
         </nav>
     )
