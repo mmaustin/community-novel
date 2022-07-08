24fbde200e489cee44633124b3d9c891298e7f24
McCray Austin <47298611+mmaustin@users.noreply.github.com>
2022-07-08 15:40:58 -0400

    corrected the placement of the menu container div

--- cn-client/src/app/Navbar.js
+++ cn-client/src/app/Navbar.js
@@ -6,13 +6,15 @@ export const Navbar = () =>{
     return(
         <nav id='navbar'>
             <h4 id='nav-header'>Navigate To Authors, Works & Contributions!</h4>
-            <div className='dropdown'>
-                <div className='button-container'><button className='dropbtn'>Menu</button></div>
-                <div className='dropdown-content'>
-                    <Link className='component-link' to='/'>Home Page</Link>
-                    <Link className='component-link' to='/all-authors'>The Authors</Link>
-                    <Link className='component-link' to='/all-works'>The Works</Link>
-                    <Link className='component-link' to='/all-contributions'>The Contributions</Link>                
+            <div className='menu-container'>
+                    <div className='dropdown'>
+                    <button className='dropbtn'>Menu</button>
+                    <div className='dropdown-content'>
+                        <Link className='component-link' to='/'>Home Page</Link>
+                        <Link className='component-link' to='/all-authors'>The Authors</Link>
+                        <Link className='component-link' to='/all-works'>The Works</Link>
+                        <Link className='component-link' to='/all-contributions'>The Contributions</Link>                
+                    </div>
                 </div>
             </div>
         </nav>
