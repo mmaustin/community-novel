30e91cacdc24596e790f02ed155005ddfb26ed72
McCray Austin <47298611+mmaustin@users.noreply.github.com>
2022-07-11 16:38:12 -0400

    added class names to author list elements

--- cn-client/src/features/authors/AuthorsList.js
+++ cn-client/src/features/authors/AuthorsList.js
@@ -13,19 +13,19 @@ export const AuthorsList = () => {
     const orderedAuthors = authors.slice().sort((a,b) => a.name.localeCompare(b.name))
 
     const content = orderedAuthors.map((author, i) => {
-        return <div key={i}>
-            <p>{author.name}</p>
+        return <div className='author-list-container' key={i}>
+            <p className='author-list-name'>{author.name}</p>
             <AuthorWorks works={author.works}/>
             {/*<AuthorContributions contributions={author.contributions}/>*/}
-            <Link to={`/get-author/${author.id}`} className="author-link">Single Author</Link>
+            <Link to={`/get-author/${author.id}`} className="all-links">Single Author</Link>
         </div>
     })
 
     return(
         <>
             {content}
-            <div><Link to={'/all-works'}>All The Works!</Link></div>
-            <div><Link to={'/add-author'}>Create Your Author Profile</Link></div>
+            <div><Link className='all-links' to={'/all-works'}>All The Works!</Link></div>
+            <div><Link className='all-links' to={'/add-author'}>Create Your Author Profile</Link></div>
         </>
     )
 }
