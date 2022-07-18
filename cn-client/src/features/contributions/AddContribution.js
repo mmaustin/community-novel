1e221567287ff54973985f232e1e81bb9fb9433e
McCray Austin <47298611+mmaustin@users.noreply.github.com>
2022-07-18 13:20:40 -0400

    corrected issue with setting selecting the proper author's id as author_id in create contribution function

--- cn-client/src/features/contributions/AddContribution.js
+++ cn-client/src/features/contributions/AddContribution.js
@@ -23,13 +23,13 @@ export const AddContribution = ({work, authors}) => {
     console.log(authorId)
 
     const canSave =
-    [text].every(Boolean) && addRequestStatus === 'idle'
+    [text, authorId].every(Boolean) && addRequestStatus === 'idle'
 
     const saveContribution = async () => {
         if (canSave) {
           try {
             setAddRequestStatus('pending')
-            await dispatch(createContribution({contribution: { text, author_id: work.author_id, work_id: work.id }})).unwrap()
+            await dispatch(createContribution({contribution: { text, author_id: authorId, work_id: work.id }})).unwrap()
             setText('');
           } catch (err) {
             console.error('Failed to save the contribution: ', err)
