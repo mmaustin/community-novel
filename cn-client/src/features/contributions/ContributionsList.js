a200e38c458a79efb112038630dd6b2ea4c1af62
McCray Austin <47298611+mmaustin@users.noreply.github.com>
2022-07-21 14:14:12 -0400

    added the work's title to the contribution list information display

--- cn-client/src/features/contributions/ContributionsList.js
+++ cn-client/src/features/contributions/ContributionsList.js
@@ -7,14 +7,19 @@ export const ContributionsList = () => {
 
     const contributions = useSelector(allContributions);
     const authors = useSelector(state => state.authors.authors)
+    const works = useSelector(allWorks);
+    
 
     let contributionAuthor;
+    let contributionWork;
 
     const contributionsList = contributions.map((cont, i) => {
-        contributionAuthor = authors.find(author => author.id === cont.author_id)
+        contributionAuthor = authors.find(author => author.id === cont.author_id);
+        contributionWork = works.find(work => work.id === cont.work_id);   
         return <div key={i}>
             <p>{cont.text}</p>
             <p>{contributionAuthor.name}</p>
+            <p>{contributionWork.title}</p>
         </div>
 })
 
