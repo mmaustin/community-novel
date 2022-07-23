3dacbb030f3407a627699f80c487c326d28196ca
McCray Austin <47298611+mmaustin@users.noreply.github.com>
2022-07-23 16:34:29 -0400

    imported necessary hooks and selector function to single contribution component

--- cn-client/src/features/contributions/SingleContribution.js
+++ cn-client/src/features/contributions/SingleContribution.js
@@ -1,2 +1,13 @@
 import React from 'react';
 import { useSelector } from 'react-redux';
+import { useParams } from 'react-router-dom';
+import { getContributionById } from './contributionSlice';
+
+export const SingleContribution = () => {
+
+
+    return(
+        <>
+        </>
+    )
+}
