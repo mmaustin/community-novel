2305704120fc9066a5987a40bba7270cb8b30f68
McCray Austin <47298611+mmaustin@users.noreply.github.com>
2022-07-28 08:57:54 -0400

    removed conditional rendering of author name and update author link in single page component

--- cn-client/src/features/authors/SingleAuthorPage.js
+++ cn-client/src/features/authors/SingleAuthorPage.js
@@ -23,8 +23,8 @@ export const SingleAuthorPage = () => {
     return(
         <div id='author-page-container'>
             <div id='conditional-author-container'>
-                {author ? <p id='single-author-name'>{author.name}</p> : <p>Nope!</p>}
-                {author ? <Link className='all-links' to={`/update-author/${author.id}`} >Edit Author</Link> : <p></p>}
+                {/*{author ? <p id='single-author-name'>{author.name}</p> : <p>Nope!</p>}
+                {author ? <Link className='all-links' to={`/update-author/${author.id}`} >Edit Author</Link> : <p></p>}*/}
                 <p><button onClick={deleteThisAuthor}>Delete Author</button></p>                 
             </div>
             < CreateAuthorsWork author={author}/>        
