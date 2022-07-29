c7cda763460f441b6d9af25e92265c8b0637de57
McCray Austin <47298611+mmaustin@users.noreply.github.com>
2022-07-29 16:34:24 -0400

    changed width and background color of individual contribution containers

--- cn-client/src/features/contributions/ContributionsList.js
+++ cn-client/src/features/contributions/ContributionsList.js
@@ -21,7 +21,7 @@ export const ContributionsList = () => {
             <p className='contribution-text'>{cont.text.substring(0, 10)}</p>
             <p className='contribution-author'>Author: {contributionAuthor.name}</p>
             <p className='contribution-work'>Work: {contributionWork.title}</p>
-            <Link to={`/get-contribution/${cont.id}`} className="all-links">Contribution Here!</Link>            
+            <p><Link to={`/get-contribution/${cont.id}`} className="all-links">Full Contribution!</Link></p>          
         </div>
 })
 
