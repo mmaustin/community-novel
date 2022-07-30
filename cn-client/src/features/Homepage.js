0be5341885c0e7334c7b7d5b0939a83f15ddd453
McCray Austin <47298611+mmaustin@users.noreply.github.com>
2022-07-30 15:44:38 -0400

    imported all authors selector function and logged authors array if dispatch is successful

--- cn-client/src/features/Homepage.js
+++ cn-client/src/features/Homepage.js
@@ -3,10 +3,12 @@ import { useDispatch, useSelector } from 'react-redux';
 import { fetchAuthors } from './authors/authorSlice';
 import { fetchWorks } from './works/workSlice';
 import { fetchContributions } from './contributions/contributionSlice';
+import { allAuthors } from './authors/authorSlice';
 
 export const Homepage = () => {
 
     const dispatch = useDispatch();
+    const authors = useSelector(allAuthors);
     const authorsStatus = useSelector(state => state.authors.status);
     const worksStatus = useSelector(state => state.works.status);
     const contributionsStatus = useSelector(state => state.contributions.status);
@@ -19,6 +21,12 @@ export const Homepage = () => {
             dispatch(fetchContributions());
         }
     }, [authorsStatus, worksStatus, contributionsStatus, dispatch])
+
+    let authorsCollection;
+
+    if(authorsStatus === 'succeeded'){
+        console.log(authors)
+    }
     
     return(
         <div className='homepage-container'>
