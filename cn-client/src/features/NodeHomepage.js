20d62cf65848f3b17796a2969e9200f38242ac05
McCray Austin <47298611+mmaustin@users.noreply.github.com>
2022-08-06 14:55:19 -0400

    partial set up of fetch request from node server

--- cn-client/src/features/NodeHomepage.js
+++ cn-client/src/features/NodeHomepage.js
@@ -1,6 +1,12 @@
-import React, {useState} from 'react';
+import React, {useState, useEffect} from 'react';
 
 export const NodeHomepage = () => {
+
+    useEffect(() => {
+        const fetchData = async () => {
+            const data = await fetch('http://localhost:3001/api/v1/auth')
+        }
+    }, [])
 
 
     return(
