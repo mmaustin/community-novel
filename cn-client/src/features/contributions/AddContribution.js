8f5e8ccafd68a709d84cf339c5cd26a96f7b3861
McCray Austin <47298611+mmaustin@users.noreply.github.com>
2022-08-07 14:41:55 -0400

    added e.prevent default to save contribution function

--- cn-client/src/features/contributions/AddContribution.js
+++ cn-client/src/features/contributions/AddContribution.js
@@ -25,12 +25,12 @@ export const AddContribution = ({work, authors}) => {
     const canSave =
     [text, authorId].every(Boolean) && addRequestStatus === 'idle'
 
-    const saveContribution = async () => {
+    const saveContribution = async (e) => {
+      e.preventDefault();
         if (canSave) {
           try {
             setAddRequestStatus('pending')
-            const a = await dispatch(createContribution({contribution: { text, author_id: authorId, work_id: work.id }})).unwrap()
-            console.log(a);
+            await dispatch(createContribution({contribution: { text, author_id: authorId, work_id: work.id }})).unwrap()
             setText('');
           } catch (err) {
             console.error('Failed to save the contribution: ', err)
