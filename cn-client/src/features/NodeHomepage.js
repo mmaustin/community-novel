429412677dd5deade0ce449972588b71a77a5e62
McCray Austin <47298611+mmaustin@users.noreply.github.com>
2022-08-07 13:09:28 -0400

    attempted use of fetch/then syntax to fix failure to retrieve data

--- cn-client/src/features/NodeHomepage.js
+++ cn-client/src/features/NodeHomepage.js
@@ -3,14 +3,16 @@ import React, {useEffect} from 'react';
 export const NodeHomepage = () => {
 
     useEffect(() => {
-        const fetchData = async () => {
-            const d = await fetch('http://localhost:5001/api/v1/products', {
+        const fetchData = () => {
+            fetch('http://localhost:5001/api/v1/products', {
                 mode: 'no-cors',
                 headers: {
                 //'Access-Control-Allow-Origin':'*',
                 }
             })
-            console.log(d);
+            .then((response) => response.json())
+            .then((products) => console.log(products));
+            
         }
         fetchData();
     }, [])
